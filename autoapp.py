f400ce8b3ee4aa3490238826f7d6700685208ad7
yiwen song <yiwensong.com@gmail.com>
2018-02-26 07:51:50 +0000

    cleaned up commented code and added uwsgi magic for later

--- autoapp.py
+++ autoapp.py
@@ -3,11 +3,7 @@
 from flask.helpers import get_debug_flag
 
 from yiwen_dot_date.routes import app
-# from yiwen_dot_date.settings import DevConfig, ProdConfig
-
-# CONFIG = DevConfig if get_debug_flag() else ProdConfig
-
-# app = create_app(CONFIG)
+application = app # uwsgi magic
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000)
